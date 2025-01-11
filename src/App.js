44dcc1e6235ae32201e0994054329a52923a9e04
anushaavulapati <anusha.avulapati@gmail.com>
2025-01-11 15:50:50 -0500

    Added basename for React Router

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/MyPortfolio">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
